c97aaeb4043aa0d101c023ea7d37ffb5d2d0677f
Chris Stroud <chris.stroud@hiring-hub.com>
2022-01-11 22:18:29 +0000

    Fix google font link

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,13 +12,9 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Rubik:ital@0;1&display=optional"
+            href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500&display=swap"
             rel="stylesheet"
           />
 
